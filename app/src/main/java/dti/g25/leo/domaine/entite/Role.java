fbebce073c41e45066114ebef741a67e56c6e72f
Philippe Joubarne <pjoubarne@dept-info.crosemont.quebec>
2020-04-23 16:32:16 -0400

    Création de l'enum Role

---
+++ app/src/main/java/dti/g25/leo/domaine/entite/Role.java
@@ -0,0 +1,13 @@
+package dti.g25.leo.domaine.entite;
+
+/**
+ Enum qui contient les differents role d'un utilisateur
+ @author Philippe Joubarne
+ @since 23/04/2019
+ @version 1
+ */
+
+public enum Role {
+    ENSEIGNANT,
+    ETUDIANT;
+}
