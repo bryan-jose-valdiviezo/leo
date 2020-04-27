f7d87534b011db352c4a478f21fbaf6e371a1ac2
Philippe Joubarne <philippejoubarne2@gmail.com>
2020-04-27 16:41:35 -0400

    Correction des javaDoc pour EtatSeance et Role Correction des javaDoc pour Seance, enleve des espaces entre javaDoc et fonction, enleve de l'espace dans les imports, modification des attributs de protected à private

--- app/src/main/java/dti/g25/leo/domaine/entite/EtatSeance.java
+++ app/src/main/java/dti/g25/leo/domaine/entite/EtatSeance.java
@@ -1,12 +1,11 @@
 package dti.g25.leo.domaine.entite;
 
 /**
- Enum qui contient les differents états d'une séance
- @author Philippe Joubarne
- @since 23/04/2019
- @version 1
+ * Enum qui contient les differents états d'une séance
+ * @author Philippe Joubarne
+ * @since 23/04/2019
+ * @version 1
  */
-
 public enum EtatSeance {
     PREVU,
     TENUE,
--- app/src/main/java/dti/g25/leo/domaine/entite/Role.java
+++ app/src/main/java/dti/g25/leo/domaine/entite/Role.java
@@ -1,12 +1,11 @@
 package dti.g25.leo.domaine.entite;
 
 /**
- Enum qui contient les differents role d'un utilisateur
- @author Philippe Joubarne
- @since 23/04/2019
- @version 1
+ * Enum qui contient les differents role d'un utilisateur
+ * @author Philippe Joubarne
+ * @since 23/04/2019
+ * @version 1
  */
-
 public enum Role {
     ENSEIGNANT,
     ETUDIANT;
