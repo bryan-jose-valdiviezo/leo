231451cfc984bf38313565f68ce4eca17e8fa376
Philippe Joubarne <philippejoubarne2@gmail.com>
2020-04-27 16:47:07 -0400

    Correction des javaDoc pour Utilisateur, enleve des espaces entre javaDoc et fonction, enleve de l'espace dans les imports, modification des attributs de protected à private

--- app/src/main/java/dti/g25/leo/domaine/entite/Utilisateur.java
+++ app/src/main/java/dti/g25/leo/domaine/entite/Utilisateur.java
@@ -1,27 +1,26 @@
 package dti.g25.leo.domaine.entite;
 
-/**
- Contient les methodes et les Attributs pour un Utilisateur
- @author Philippe
- @since 23/04/2020
- @version 1
- */
 
+/**
+ * Contient les methodes et les Attributs pour un Utilisateur
+ * @author Philippe
+ * @since 23/04/2020
+ * @version 1
+ */
 public class  Utilisateur {
 
-    protected int id;
-    protected String prenom;
-    protected String nom;
-    protected Role role;
+    private int id;
+    private String prenom;
+    private String nom;
+    private Role role;
 
     /**
-     Initialise un utilisateur avec son id, son prénom, son nom et son rôle
-     @param unId : int
-     @param unPrenom : String
-     @param unNom : String
-     @param unRole : String
+     * Initialise un utilisateur avec son id, son prénom, son nom et son rôle
+     * @param unId : int
+     * @param unPrenom : String
+     * @param unNom : String
+     * @param unRole : String
      */
-
     public Utilisateur(int unId, String unPrenom, String unNom, Role unRole){
         assert unNom != null && unNom != "" : "Le nom de l'utilisateur ne peut être nulle ou vide";
 
@@ -32,61 +31,45 @@ public class  Utilisateur {
     }
 
     /**
-     Constructeur minimal d'un utilisateur
-     @param unId : int
+     * Retourne l'id de l'utilisateur
+     * @return id : int
      */
-    public Utilisateur(int unId) {
-
-        id = unId;
-    }
-
-    /**
-     Retourne l'id de l'utilisateur
-     @return id : int
-     */
-
     public int getId(){ return id; }
 
     /**
-     Retourne le prenom de l'utilisateur
-     @return prenom : int
+     * Retourne le prenom de l'utilisateur
+     * @return prenom : int
      */
-
     public String getPrenom(){ return prenom; }
 
     /**
-     Retourne le nom de l'utilisateur
-     @return nom : String
+     * Retourne le nom de l'utilisateur
+     * @return nom : String
      */
-
     public String getNom(){ return nom; }
 
     /**
-     Retourne le rôle de l'utilisateur
-     @return role : String
+     * Retourne le rôle de l'utilisateur
+     * @return role : String
      */
-
     public Role getRole(){ return role; }
 
     /**
-     Émet un prenom à l'utilisateur
-     @param nouveauPrenom : String
+     * Émet un prenom à l'utilisateur
+     * @param nouveauPrenom : String
      */
-
     public void setPrenom(String nouveauPrenom){ prenom=nouveauPrenom; }
 
     /**
-     Émet un nom de l'utilisateur
-     @param nouveauNom : String
+     * Émet un nom de l'utilisateur
+     * @param nouveauNom : String
      */
-
     public void setNom(String nouveauNom){ nom=nouveauNom; }
 
     /**
-     Émet un rôle de l'utilisateur
-     @param nouveauRole : String
+     * Émet un rôle de l'utilisateur
+     * @param nouveauRole : String
      */
-
     public void setRole(Role nouveauRole){ role=nouveauRole; }
 
 
