af0e77c6745a372e695787f7d9c53f0cb4029086
Simon Roy <simon.roy1211@gmail.com>
2020-04-30 16:13:06 -0400

    Ajout du menu navigation

--- app/src/main/java/dti/g25/leo/ui/activite/ActiviteChoixUtilisateurs.java
+++ app/src/main/java/dti/g25/leo/ui/activite/ActiviteChoixUtilisateurs.java
@@ -1,10 +1,12 @@
 package dti.g25.leo.ui.activite;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import dti.g25.leo.R;
+import dti.g25.leo.presentation.vue.VueBase;
 
 public class ActiviteChoixUtilisateurs extends AppCompatActivity {
 
@@ -13,6 +15,8 @@ public class ActiviteChoixUtilisateurs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix_utilisateurs);
 
-
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.layout_authentification, new VueBase());
+        ft.commit();
     }
 }
